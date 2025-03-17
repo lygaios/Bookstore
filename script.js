a5aae6178ac5bd9061bcacdbe147ad4fb8afa4f5
Joshua Brunke <seafortghost@gmail.com>
2025-03-17 13:18:26 +0100

    added display of basic data

--- script.js
+++ script.js
@@ -12,12 +12,20 @@ for (let i = 0; i < books.length; i++) {
 function getBookTemplate(i) {
     return ` 
     <div class="book-box">
-        <div class="data-block"></div>
+        <div class="data-block">
+            <h2>${books[i].title}</h2>
+            <p>${books[i].author}</p>
+            <p>${books[i].price.toFixed(2)} €</p>
+            <p>${books[i].publishedYear}</p>
+            <p>${books[i].genre}</p>
+            
+        </div>
         <div class="like-row"></div>
         <div class="comment-box"></div>
     </div>
     `
 }
+
 
 function addComment(i) {
 
