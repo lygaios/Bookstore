34c41dc12d1854297d9cf2cf09d6656785371d55
Joshua Brunke <seafortghost@gmail.com>
2025-03-17 20:00:03 +0100

    Added various fields and functions, but not functional yet

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ function init() {
 }
 
 function renderBooks() {
-document.getElementById("main").innerHTML = "";
-for (let i = 0; i < books.length; i++) {
-    main.innerHTML += getBookTemplate(i);
-};
+    let bookRef = document.getElementById("main");
+    bookRef.innerHTML = "";
+    for (let i = 0; i < books.length; i++) {
+    bookRef.innerHTML += getBookTemplate(i);
+
+    let commentsRef = document.getElementById(`comments${j}`)
     for (let j = 0; j < books[i].comments.length; j++) {
-        `comments${j}`.innerHTML += getCommentsTemplate(i, j);
+        commentsRef.innerHTML += getCommentsTemplate(i, j);
     };
     }
-
+}
 
 function getBookTemplate(i) {
     return ` 
@@ -23,25 +25,47 @@ function getBookTemplate(i) {
             <p>${books[i].publishedYear}</p>
             <p>${books[i].genre}</p>
         </div>
-        <div class="like-row"></div>
+        
+        <div class="like-row">
+        <p>${books[i].likes}</p> 
+        <img class="heart" src="./assets/icons/heart.png" alt="Like" onclick="addLike(i)">
+        </div>
+        
         <div class="comment-box">
             <h3>Kommentare</h3>
             <div class="input-field">
                 <p>Name:</p>    
-                <input id="name-input" class="text-input" type="text"></input> 
+                <input id="name-input${j}" class="text-input" type="text"></input> 
                 <p>Kommentar:</p>    
-                <input id="comment-input" class="text-input" type="text"></input> 
+                <input id="comment-input${j}" class="text-input" type="text"></input> 
                 <button class="button" onclick="addComment()">Kommentar absenden</button>
             </div>
+            <div class="comments" id="comments${j}"></div>
         </div>
     </div>
     `
 }
 
-function getCommentsTemplate(i, j) {
+function addLike(i) {
+let likesNumber = document.getElementById(`${books[i].likes}`);
+likesNumber = `${books[i].likes + 1}`;
+addClass="liked-status";
+}
 
+function getCommentsTemplate(i, j) {
+    return `
+    <p>${books[i].comments[j].name}: ${books[i].comments[j].comment}</p>
+    `
 }
 
 function addComment(i, j) {
-
+    let nameInput = document.getElementById("name-input${j}");
+    let commentInput = document.getElementById("comment-input${j}");
+    if (nameInput.value != "" && commentInput.value != "") {
+        books[i].comments[j].name.push(nameInput.value);
+        books[i].comments[j].comment.push(commentInput.value);
+    }
+    renderBooks(i);
+    nameInput.value = "";
+    commentInput.value = "";
 }
