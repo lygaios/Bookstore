37468cc8c46dfa1cace083794512867d6120318c
Joshua Brunke <seafortghost@gmail.com>
2025-03-17 17:41:09 +0100

    Added first structure for comment section

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ function renderBooks() {
 document.getElementById("main").innerHTML = "";
 for (let i = 0; i < books.length; i++) {
     main.innerHTML += getBookTemplate(i);
+};
+    for (let j = 0; j < books[i].comments.length; j++) {
+        `comments${j}`.innerHTML += getCommentsTemplate(i, j);
+    };
     }
-}
+
 
 function getBookTemplate(i) {
     return ` 
@@ -18,15 +22,26 @@ function getBookTemplate(i) {
             <p>${books[i].price.toFixed(2)} €</p>
             <p>${books[i].publishedYear}</p>
             <p>${books[i].genre}</p>
-            
         </div>
         <div class="like-row"></div>
-        <div class="comment-box"></div>
+        <div class="comment-box">
+            <h3>Kommentare</h3>
+            <div class="input-field">
+                <p>Name:</p>    
+                <input id="name-input" class="text-input" type="text"></input> 
+                <p>Kommentar:</p>    
+                <input id="comment-input" class="text-input" type="text"></input> 
+                <button class="button" onclick="addComment()">Kommentar absenden</button>
+            </div>
+        </div>
     </div>
     `
 }
 
+function getCommentsTemplate(i, j) {
 
-function addComment(i) {
+}
+
+function addComment(i, j) {
 
 }
