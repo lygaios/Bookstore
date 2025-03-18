f56f90cd3b958e171f531da1c1b2ce47885dfec3
Joshua Brunke <seafortghost@gmail.com>
2025-03-18 13:19:40 +0100

    Fixed comment and like functions, tidied up code

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 function init() {
-    renderBooks();
+  renderBooks();
 }
 
 function renderBooks() {
-    let booksRef = document.getElementById("main");
-    booksRef.innerHTML = "";
-    for (let i = 0; i < books.length; i++) {
+  let booksRef = document.getElementById("main");
+  booksRef.innerHTML = "";
+  for (let i = 0; i < books.length; i++) {
     booksRef.innerHTML += getBookTemplate(i);
-
+    heartShadow(i);
     let commentsRef = document.getElementById(`comments${i}`);
     commentsRef.innerHTML = "";
     for (let j = 0; j < books[i].comments.length; j++) {
-    commentsRef.innerHTML += getCommentsTemplate(i, j);
-    };
+      commentsRef.innerHTML += getCommentsTemplate(i, j);
     }
+  }
+}
+
+function heartShadow(i) {
+  let heartRef = document.getElementById(`hearticon${i}`);
+  if (books[i].liked == true) {
+    heartRef.classList.add("liked-shadow");
+  } else {
+    heartRef.classList.remove("liked-shadow");
+  }
 }
 
 function getBookTemplate(i) {
-    return ` 
+  return ` 
     <div class="book-box">
         <div class="data-block">
             <h2>${books[i].title}</h2>
@@ -29,7 +38,7 @@ function getBookTemplate(i) {
         
         <div class="like-row">
         <p>${books[i].likes}</p> 
-        <img class="heart" src="./assets/icons/heart.png" alt="Like" onclick="addLike(${i})">
+        <img class="heart" src="./assets/icons/heart.png" id="hearticon${i}" alt="Like" onclick="addLike(${i})">
         </div>
         
         <div class="comment-box">
@@ -44,44 +53,37 @@ function getBookTemplate(i) {
             <div class="comments" id="comments${i}"></div>
         </div>
     </div>
-    `
+    `;
 }
 
 function addLike(i) {
-    if (books[i].liked == false) {
-        let likesNumber = books[i].likes + 1;
-        books[i].likes = likesNumber;
-        
-        addClass="liked-status";
-        books[i].liked = true;
-        renderBooks();
-    
-  
-    } else {
-        let likesNumber = books[i].likes - 1;
-        books[i].likes = likesNumber;
-        removeClass="liked-status";
-        books[i].liked = false;
-        renderBooks();
-    }
-  
+  if (books[i].liked == false) {
+    let likesNumber = books[i].likes + 1;
+    books[i].likes = likesNumber;
+    books[i].liked = true;
+    renderBooks();
+  } else {
+    let likesNumber = books[i].likes - 1;
+    books[i].likes = likesNumber;
+    books[i].liked = false;
+    renderBooks();
+  }
 }
 
 function getCommentsTemplate(i, j) {
-    return `
+  return `
     <p>${books[i].comments[j].name}: ${books[i].comments[j].comment}</p>
-    `
+    `;
 }
 
 function addComment(i) {
-    let nameInput = document.getElementById(`name-input${i}`);
-    let commentInput = document.getElementById(`comment-input${i}`);
-    if (nameInput.value != "" && commentInput.value != "") {
-        let newComment = {name: nameInput.value, comment: commentInput.value};
-        books[i].comments.push(newComment); 
-
+  let nameInput = document.getElementById(`name-input${i}`);
+  let commentInput = document.getElementById(`comment-input${i}`);
+  if (nameInput.value != "" && commentInput.value != "") {
+    let newComment = { name: nameInput.value, comment: commentInput.value };
+    books[i].comments.push(newComment);
     renderBooks();
     nameInput.value = "";
     commentInput.value = "";
-    }
+  }
 }
